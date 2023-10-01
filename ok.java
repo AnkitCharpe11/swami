4a970b5f09501db1aaf58b218be7877e965fda98
AnkitCharpe11 <ankitcharpe11@gmail.com>
2023-10-01 20:49:19 +0530

    modified by ankey

--- ok.java
+++ ok.java
@@ -6,3 +6,6 @@ class done{
         System.out.println("git stash command we are going to lern today from the first time ");
     }
 }
+
+ankit== brilient if you do consistancy anywhere else 
+sumit == moneyMaaker
