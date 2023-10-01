d0164b0e628637c6f216b998c6e17b069a382110
AnkitCharpe11 <ankitcharpe11@gmail.com>
2023-10-01 16:45:40 +0530

    edited bug fixed

---
+++ new1.java
@@ -0,0 +1,9 @@
+class new1
+{
+    public static void main (String []args)
+    {
+        System.out.println("ok");
+        System.out.println("git command goes well So you can do it Ankit Just do daily basis alway stay positive as you are ");
+
+    }
+}
