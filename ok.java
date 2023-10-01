7699ad5280dab211d1dbdc8ff8cdb0ab4409c4de
AnkitCharpe11 <ankitcharpe11@gmail.com>
2023-10-01 20:39:35 +0530

    first commit

---
+++ ok.java
@@ -0,0 +1,8 @@
+class done{
+
+    public static domethod()
+    {
+        System.out.println("we are going to lern git and github ");
+        System.out.println("git stash command we are going to lern today from the first time ");
+    }
+}
