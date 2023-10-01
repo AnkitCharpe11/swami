6eece1aa91380860055ffcddbd6ec2cdf3a40552
AnkitCharpe11 <ankitcharpe11@gmail.com>
2023-10-01 20:47:04 +0530

    modified done

---
+++ one.java
@@ -0,0 +1,6 @@
+one jabva 
+
+pooja has a job
+gayatri have a job with 11 lpa 
+prajwal have a job with 9lpackage
+what about you ankit make some sirious stuff with you know one will help you without you wo]]]
